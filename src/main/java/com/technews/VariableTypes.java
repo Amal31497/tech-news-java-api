620b6664ce0fb04bfb3bdc736e8b0850a2a47484
Amal Janabayev <amaljanabayev@Amals-MacBook-Pro.local>
2021-09-09 16:31:20 -0700

    Created models for tech-news api learning basics of Java

---
+++ src/main/java/com/technews/VariableTypes.java
@@ -0,0 +1,15 @@
+package com.technews;
+
+public class VariableTypes {
+    public static void main(String[] args){
+        String name = "Cleopatra";
+        int age = 2016 + 69;
+        double price = 1999.99;
+        boolean truth = true;
+        boolean falseness = !truth;
+
+        double sum = age + price;
+        String addition = age + name;
+        System.out.println(addition);
+    }
+}
